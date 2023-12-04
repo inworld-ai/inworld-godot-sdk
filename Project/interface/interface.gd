c9c675e5b8d89c9d09dc62796c42d75efbf6a20a
Matt Carey <mcarey@inworld.ai>
2023-12-04 12:51:15 -0800

    add inworld player (#6)
    inworld player class to reference current character target

--- Project/interface/interface.gd
+++ Project/interface/interface.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@export var inworld_character : InworldCharacter
+@export var inworld_player : InworldPlayer
 
 signal player_text(text : String)
 signal character_text(text : String)
@@ -8,7 +8,7 @@ signal character_text(text : String)
 
 func _on_text_input_entered(text : String):
 	player_text.emit(text)
-	inworld_character.send_text(text)
+	inworld_player.target_character.send_text(text)
 
 
 func _on_inworld_got_text(text : String):
