b636e9a6530a65f3105bcf400f1b2f2eba9ef42c
vsivaraja <vikram@inworld.ai>
2023-12-04 16:32:04 -0800

    Fix

--- Project/interface/interface.gd
+++ Project/interface/interface.gd
@@ -5,13 +5,12 @@ extends Control
 signal player_text(text : String)
 signal character_text(text : String)
 
-var effect
+var effect : AudioEffectCapture
 var recording
 
 func _ready():
 	var idx = AudioServer.get_bus_index("Listen")
 	effect = AudioServer.get_bus_effect(idx, 0) as AudioEffectCapture
-
 
 func _on_text_input_entered(text : String):
 	player_text.emit(text)
@@ -22,19 +21,17 @@ func _on_inworld_got_text(text : String):
 	character_text.emit(text)
 
 func _process(delta):
-	if $Listen.is_current():
-		var num_frames = effect.get_buffer_length_frames()
-		var buf = effect.get_buffer(num_frames)
-		print(buf)
-		inworld_player.target_character.send_audio(buf)
-		effect.clear_buffer()
+	var buf = effect.get_buffer(effect.get_frames_available())
+	print(buf)
+	inworld_player.target_character.send_audio(buf)
+	effect.clear_buffer()
 
 func _on_text_input_recording():
-	$Listen.make_current()
+	$MicListener.autoplay = true
 	print("Starting audio session!")
 	inworld_player.target_character.start_audio_session()
 
 func _on_text_input_stop_recording():
-	$Listen.clear_current()
+	$MicListener.autoplay = false
 	print("Stopping audio session!")
 	inworld_player.target_character.stop_audio_session()
