81658eeec9305e22bd32afb4a3d0ddb6d1cc3991
Matt Carey <mcarey@inworld.ai>
2023-12-07 02:13:34 -0800

    improved queue (#16)
    abstract queue away from end user. simply call finish to prompt next if available for message emit

--- Inworld/src/inworld_talk_queue.cpp
+++ Inworld/src/inworld_talk_queue.cpp
@@ -5,15 +5,13 @@
 using namespace godot;
 
 void InworldTalkQueue::_bind_methods() {
-	ClassDB::bind_method(D_METHOD("is_next_ready"), &InworldTalkQueue::is_next_ready);
-	ClassDB::bind_method(D_METHOD("pop_next_ready"), &InworldTalkQueue::pop_next_ready);
+	ClassDB::bind_method(D_METHOD("finish_current"), &InworldTalkQueue::finish_current);
 
-	ADD_SIGNAL(MethodInfo("next_ready"));
-	ADD_SIGNAL(MethodInfo("next_popped", PropertyInfo(Variant::OBJECT, "talk")));
+	ADD_SIGNAL(MethodInfo("next_talk_ready", PropertyInfo(Variant::OBJECT, "talk")));
 }
 
 InworldTalkQueue::InworldTalkQueue() :
-		Object{} {
+		Object{}, utterance_ids{}, utterance_id_to_talk{}, current_talk{} {
 }
 
 InworldTalkQueue::~InworldTalkQueue() {
@@ -21,55 +19,43 @@ InworldTalkQueue::~InworldTalkQueue() {
 
 void InworldTalkQueue::update_text(String p_utterance_id, String p_text) {
 	_find_or_create(p_utterance_id)->text = p_text;
-	_check_talk_ready(p_utterance_id);
+	_check_talk_ready();
 }
 
 void InworldTalkQueue::update_chunk(String p_utterance_id, PackedByteArray p_chunk) {
 	_find_or_create(p_utterance_id)->chunk = p_chunk;
-	_check_talk_ready(p_utterance_id);
+	_check_talk_ready();
 }
 
-void InworldTalkQueue::_check_talk_ready(String p_utterance_id) {
-	if (ready_utterances.find(p_utterance_id) == -1) {
-		Ref<InworldMessageTalk> message_talk = utterance_to_talk[utterances[0]];
-		std::string str = std::to_string(message_talk->chunk.size());
-		if (message_talk->get_ready()) {
-			ready_utterances.push_back(p_utterance_id);
-			if (p_utterance_id == utterances[0]) {
-				emit_signal("next_ready");
-			}
-		}
+void InworldTalkQueue::_check_talk_ready() {
+	if (current_talk.is_valid()) {
+		return;
+	}
+	if (utterance_ids.is_empty()) {
+		return;
+	}
+	Ref<InworldMessageTalk> message_talk = utterance_id_to_talk[utterance_ids[0]];
+	if (message_talk->get_ready()) {
+		current_talk = message_talk;
+		utterance_id_to_talk.erase(utterance_ids[0]);
+		utterance_ids.remove_at(0);
+		emit_signal("next_talk_ready", current_talk);
 	}
 }
 
 Ref<InworldMessageTalk> InworldTalkQueue::_find_or_create(String p_utterance_id) {
 	Ref<InworldMessageTalk> message_talk;
-	if (utterance_to_talk.has(p_utterance_id)) {
-		message_talk = utterance_to_talk[p_utterance_id];
+	if (utterance_id_to_talk.has(p_utterance_id)) {
+		message_talk = utterance_id_to_talk[p_utterance_id];
 	} else {
 		message_talk.instantiate();
-		utterances.push_back(p_utterance_id);
-		utterance_to_talk[p_utterance_id] = message_talk;
+		utterance_ids.push_back(p_utterance_id);
+		utterance_id_to_talk[p_utterance_id] = message_talk;
 	}
 	return message_talk;
 }
 
-bool InworldTalkQueue::is_next_ready() const {
-	if (utterances.is_empty()) {
-		return false;
-	}
-	Ref<InworldMessageTalk> next = utterance_to_talk[utterances[0]];
-	return next->get_ready();
-}
-
-Ref<InworldMessageTalk> InworldTalkQueue::pop_next_ready() {
-	Ref<InworldMessageTalk> message_talk;
-	if (is_next_ready()) {
-		message_talk = utterance_to_talk[utterances[0]];
-		utterance_to_talk.erase(utterances[0]);
-		ready_utterances.erase(utterances[0]);
-		utterances.remove_at(0);
-		emit_signal("next_popped", message_talk);
-	}
-	return message_talk;
+void InworldTalkQueue::finish_current() {
+	current_talk = Ref<InworldMessageTalk>{};
+	_check_talk_ready();
 }
